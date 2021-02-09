d3df72ad978fa114b57295f04a8fdc8fdda34ddc
Talantbek Zhenishbek uulu <talantjenishbekuulu@gmail.com>
2021-02-10 00:58:33 +0600

    Error fixed

--- src/models/user.js
+++ src/models/user.js
@@ -74,7 +74,7 @@ userSchema.methods.toJSON = function() {
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = await jwt.sign({ _id: user._id.toString() }, process.env,JWT_SECRET)
+    const token = await jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
 
     user.tokens = await user.tokens.concat({ token })
     await user.save()
